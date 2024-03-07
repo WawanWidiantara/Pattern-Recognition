a6fb59765bb68ddd692c0de2f144048bd8d6d23b
WawanWidiantara <gdwidi13@gmail.com>
2024-03-07 19:17:11 +0700

    + svm scratch

--- Face-Recognition/record.py
+++ Face-Recognition/record.py
@@ -17,7 +17,7 @@ while True:
         )
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
     cv2.imshow("Capturing", frame)
-    if a > 29:
+    if a > 49:
         break
 
 video.release()
